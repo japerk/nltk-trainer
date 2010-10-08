526684266bb01bf6307291f3f3f0a1cf70d689f1
Jacob Perkins <japerk@gmail.com>
2010-10-08 08:18:47 -0700

    corpus reader creation, instance extraction, feat & eval options

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -1,6 +1,14 @@
-import argparse
+import argparse, itertools, re
 from nltk.classify import MaxentClassifier
+from nltk.corpus import stopwords
+from nltk.corpus.reader import CategorizedPlaintextCorpusReader, CategorizedTaggedCorpusReader
+from nltk.corpus.util import LazyCorpusLoader
 from nltk.metrics import BigramAssocMeasures
+from nltk.util import bigrams
+
+########################################
+## command options & argument parsing ##
+########################################
 
 parser = argparse.ArgumentParser(description='Train a NLTK Classifier')
 
@@ -19,7 +27,8 @@ corpus_group.add_argument('--reader', choices=('plaintext', 'tagged'),
 	default='plaintext',
 	help='specify categorized plaintext or part-of-speech tagged corpus')
 corpus_group.add_argument('--cat_pattern', default='(.+)/.+',
-	help='regular expression pattern to identify categories based on file paths')
+	help='''regular expression pattern to identify categories based on file paths.
+	if cat_file is also given, this pattern is used to identify corpus file ids.''')
 corpus_group.add_argument('--cat_file',
 	help='relative path to a file containing category listings')
 corpus_group.add_argument('--delimiter', default=' ',
@@ -42,7 +51,18 @@ classifier_group.add_argument('--binary', action='store_true', default=False,
 classifier_group.add_argument('--multi', action='store_true', default=False,
 	help='train a multi-class classifier, or a multi-binary classifier if --binary is also given')
 
-score_group = parser.add_argument_group('Feature Scoring')
+feat_group = parser.add_argument_group('Feature Extraction',
+	'The default is to lowercase every word and filter out stopwords')
+feat_group.add_argument('--bigrams', action='store_true', default=False,
+	help='include bigrams as features')
+feat_group.add_argument('--no-lowercase', action='store_true', default=False,
+	help="don't lowercase every word")
+feat_group.add_argument('--filter-stopwords', default='english',
+	choices=['no']+stopwords.fileids(),
+	help='stopwords to filter, or "no" if want to keep stopwords')
+
+score_group = parser.add_argument_group('Feature Scoring',
+	'The default is no scoring, all words are included as features')
 score_group.add_argument('--score_fn', default='chi_sq',
 	choices=[f for f in dir(BigramAssocMeasures) if not f.startswith('_')],
 	help='scoring function for information gain and bigram collocations')
@@ -50,6 +70,22 @@ score_group.add_argument('--min_score', default=0, type=int,
 	help='minimum score for a word to be included. if 0, all words are used.')
 score_group.add_argument('--max-feats', default=0, type=int,
 	help='maximum number of words to include, ordered by highest score. if 0, all words are used.')
+
+eval_group = parser.add_argument_group('Classifier Evaluation',
+	'''The default is to test the classifier against the unused fraction of the
+	corpus, or against the entire corpus if the whole corpus is used for training.''')
+eval_group.add_argument('--no-eval', action='store_true', default=False,
+	help="don't do any evaluation")
+eval_group.add_argument('--no-accuracy', action='store_true', default=False,
+	help="don't evaluate accuracy")
+eval_group.add_argument('--no-precision', action='store_true', default=False,
+	help="don't evaluate precision")
+eval_group.add_argument('--no-recall', action='store_true', default=False,
+	help="don't evaluate recall")
+eval_group.add_argument('--no-fmeasure', action='store_true', default=False,
+	help="don't evaluate f-measure")
+eval_group.add_argument('--no-masi-distance', action='store_true', default=False,
+	help="don't evaluate masi distance (only applies to a multi binary classifier)")
 
 maxent_group = parser.add_argument_group('Maxent Classifier',
 	'These options only apply when a Maxent algorithm is chosen.')
@@ -67,3 +103,90 @@ decisiontree_group.add_argument('--depth_cutoff', default=100, type=int)
 decisiontree_group.add_argument('--support_cutoff', default=10, type=int)
 
 args = parser.parse_args()
+
+###################
+## corpus reader ##
+###################
+
+reader_class = {
+	'plaintext': CategorizedPlaintextCorpusReader,
+	'tagged': CategorizedTaggedCorpusReader
+}
+
+reader_args = []
+reader_kwargs = {}
+
+if args.cat_file:
+	reader_kwargs['cat_file'] = args.cat_file
+	
+	if args.delimiter and args.delimiter != ' ':
+		reader_kwargs['delimiter'] = args.delimiter
+	
+	if args.cat_pattern:
+		reader_args.append(args.cat_pattern)
+	else:
+		reader_args.append('.+/.+')
+elif args.cat_pattern:
+	reader_args.append(args.cat_pattern)
+	reader_kwargs['cat_pattern'] = re.compile(args.cat_pattern)
+
+categorized_corpus = LazyCorpusLoader(args.corpus, reader_class[args.reader],
+	*reader_args, **reader_kwargs)
+labels = categorized_corpus.categories()
+nlabels = len(labels)
+
+if args.trace:
+	print '%d labels: %s' % (nlabels, labels)
+
+if not nlabels:
+	raise ValueError('corpus does not have any categories')
+elif nlabels == 1:
+	raise ValueError('corpus must have more than 1 category')
+elif nlabels == 2 and args.multi:
+	raise ValueError('corpus must have more than 2 categories if --multi is specified')
+
+#####################
+## text extraction ##
+#####################
+
+if args.filter_stopwords == 'no':
+	stopset = set()
+else:
+	stopset = set(stopwords.words(args.filter_stopwords))
+
+def norm_sent(words):
+	if not args.no_lowercase:
+		words = [w.lower() for w in words]
+	
+	if stopset:
+		words = [w for w in words if w.lower() not in stopset]
+	
+	if args.bigrams:
+		return words + bigrams(words)
+	else:
+		return words
+
+def label_sents(label):
+	for sent in categorized_corpus.sents(categories=[label]):
+		yield norm_sent(sent)
+
+def label_paras(label):
+	for para in categorized_corpus.paras(categories=[label]):
+		yield itertools.chain([norm_sent(sent) for sent in para])
+
+def label_files(label):
+	for fileid in categorized_corpus.fileids(categories=[label]):
+		sents = categorized_corpus.sents(fileids=[fileid])
+		yield itertools.chain([norm_sent(sent) for sent in sents])
+
+label_instance_function = {
+	'sents': label_sents,
+	'paras': label_paras,
+	'files': label_files
+}
+
+lif = label_instance_function[args.instances]
+label_words = {}
+
+for label in labels:
+	label_words[label] = lif(label)

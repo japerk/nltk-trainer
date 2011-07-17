5215d339795e282b16688282c6f62ca65608b488
Jacob Perkins <japerk@gmail.com>
2011-07-17 09:09:09 -0700

    prepare for 1.0 release
    --HG--
    rename : README.rst => README.txt

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
+import os.path
 from setuptools import setup
+
+def open_file(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname))
 
 setup(
 	name='nltk-trainer',
-	version='0.5',
+	packages=['nltk_trainer'],
+	version='0.9',
+	description='Train NLTK objects with 0 code',
+	long_description=open_file('README.txt').read(),
+	license='Apache',
 	author='Jacob Perkins',
 	author_email='japerk@gmail.com',
 	url='https://github.com/japerk/nltk-trainer',
-	packages=['nltk_trainer'],
+	# TODO: download_url
+	keywords=['nltk', 'nlp', 'nlproc'],
 	scripts=(
-		'analyze_tagged_corpus.py', 'analyze_tagger_coverage.py',
-		'train_chunker.py', 'train_classifier.py', 'train_tagger.py',
+		'analyze_chunked_corpus.py',
+		'analyze_chunker_coverage.py',
+		'analyze_classifier_coverage.py',
+		'analyze_tagged_corpus.py',
+		'analyze_tagger_coverage.py',
+		'combine_classifiers.py',
+		'train_chunker.py',
+		'train_classifier.py',
+		'train_tagger.py',
 	),
-	license='LICENSE',
-	description='Train NLTK objects with 0 code',
-	long_description=open('README.rst').read(),
-	install_requires=(
-		'argparse>=1.1',
-		'nltk>=2.0b8',
-		'numpy==1.3.0',
-		'scipy==0.7.0',
-	)
+	install_requires=[l.strip() for l in open_file('requirements.txt').readlines()],
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2.7',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Text Processing :: Linguistic',
+	]
 )

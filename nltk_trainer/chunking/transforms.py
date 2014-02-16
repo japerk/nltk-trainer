2b687ba4d9aa90453c847ccdad47634f5635dddb
Jacob Perkins <japerk@gmail.com>
2014-02-16 09:43:37 -0800

    use node_label for tree flattening
    --HG--
    branch : py3

--- nltk_trainer/chunking/transforms.py
+++ nltk_trainer/chunking/transforms.py
@@ -12,7 +12,7 @@ def flatten_deeptree(tree):
 	>>> flatten_deeptree(Tree('S', [Tree('NP-SBJ', [Tree('NP', [Tree('NNP', ['Pierre']), Tree('NNP', ['Vinken'])]), Tree(',', [',']), Tree('ADJP', [Tree('NP', [Tree('CD', ['61']), Tree('NNS', ['years'])]), Tree('JJ', ['old'])]), Tree(',', [','])]), Tree('VP', [Tree('MD', ['will']), Tree('VP', [Tree('VB', ['join']), Tree('NP', [Tree('DT', ['the']), Tree('NN', ['board'])]), Tree('PP-CLR', [Tree('IN', ['as']), Tree('NP', [Tree('DT', ['a']), Tree('JJ', ['nonexecutive']), Tree('NN', ['director'])])]), Tree('NP-TMP', [Tree('NNP', ['Nov.']), Tree('CD', ['29'])])])]), Tree('.', ['.'])]))
 	Tree('S', [Tree('NP', [('Pierre', 'NNP'), ('Vinken', 'NNP')]), (',', ','), Tree('NP', [('61', 'CD'), ('years', 'NNS')]), ('old', 'JJ'), (',', ','), ('will', 'MD'), ('join', 'VB'), Tree('NP', [('the', 'DT'), ('board', 'NN')]), ('as', 'IN'), Tree('NP', [('a', 'DT'), ('nonexecutive', 'JJ'), ('director', 'NN')]), Tree('NP-TMP', [('Nov.', 'NNP'), ('29', 'CD')]), ('.', '.')])
 	'''
-	return Tree(tree.node, flatten_childtrees([c for c in tree]))
+	return Tree(node_label(tree), flatten_childtrees([c for c in tree]))
 
 def flatten_childtrees(trees):
 	children = []
@@ -21,7 +21,7 @@ def flatten_childtrees(trees):
 		if t.height() < 3:
 			children.extend(t.pos())
 		elif t.height() == 3:
-			children.append(Tree(t.node, t.pos()))
+			children.append(Tree(node_label(t), t.pos()))
 		else:
 			children.extend(flatten_childtrees([c for c in t]))
 	
@@ -38,6 +38,6 @@ def shallow_tree(tree):
 		if t.height() < 3:
 			children.extend(t.pos())
 		else:
-			children.append(Tree(t.node, t.pos()))
+			children.append(Tree(node_label(t), t.pos()))
 	
-	return Tree(tree.node, children)
+	return Tree(node_label(tree), children)

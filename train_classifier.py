5cd9821d44940122a7be00453628dd3c2d6e67e3
Mahmoud Lababidi <mlababidi@Mahmouds-MacBook-Pro.local>
2014-07-09 12:54:15 -0400

    fixed file output bug, if corpus is ../some_dir/corpus, pickled classifier is in wrong folder

--- train_classifier.py
+++ train_classifier.py
@@ -419,7 +419,12 @@ if not args.no_pickle:
 	if args.filename:
 		fname = os.path.expanduser(args.filename)
 	else:
-		name = '%s_%s.pickle' % (args.corpus, '_'.join(args.classifier))
+		corpus_clean = args.corpus
+		if corpus_clean[-1] == '/':
+			corpus_clean = corpus_clean[:-1]
+		import re
+		re.sub('.*/','',corpus_clean)
+		name = '%s_%s.pickle' % (corpus_clean, '_'.join(args.classifier))
 		fname = os.path.join(os.path.expanduser('~/nltk_data/classifiers'), name)
 	
 	dump_object(classifier, fname, trace=args.trace)
